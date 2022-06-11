e704c53ab8d6dc3fe25cc42f7e778b07855c0865
Lucas Rocha <13350752+lucascudo@users.noreply.github.com>
2022-06-11 15:53:43 +0100

    nothing to see here

--- src/environments/environment copy.ts
+++ src/environments/environment copy.ts
@@ -8,7 +8,7 @@ export const environment = {
     appId: '1:1066618490440:web:2d67e6378b76a78791527d',
     storageBucket: 'ubi-bike-7866d.appspot.com',
     locationId: 'europe-west',
-    apiKey: 'AIzaSyDr8ceTvvks70dfnJha8N0s-ZdO0p-CinU',
+    apiKey: 'API KEY HERE',
     authDomain: 'ubi-bike-7866d.firebaseapp.com',
     messagingSenderId: '1066618490440',
   },
